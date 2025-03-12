26ad3ed0ad9d20054dc4f3cf267993d349c116c8
AnmolSingh7e8 <anmolpreet.singh.7e8@itb.cat>
2025-03-12 09:51:46 +0100

    Exercici 3 i 4

--- app/src/main/java/com/example/exercicisstateless/MainActivity.kt
+++ app/src/main/java/com/example/exercicisstateless/MainActivity.kt
@@ -5,19 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.VerticalAlign
 import com.example.exercicisstateless.ui.theme.ExercicisStatelessTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +45,7 @@ class MainActivity : ComponentActivity() {
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
 //                }
-                Exercici1()
+                Exercici3()
             }
         }
     }
@@ -47,8 +60,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Exercici1(){
-    Column (modifier = Modifier.fillMaxSize()) {
+fun Exercici1() {
+    Column(modifier = Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(id = R.drawable.bg_compose_background),
             contentDescription = "Exemple",
@@ -69,12 +82,159 @@ fun Exercici1(){
             modifier = Modifier.padding(5.dp)
         )
     }
+}
 
+@Composable
+fun Exercici3(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        Row(
+            modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFEADDFF)).padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(Modifier.height(8.dp).fillMaxWidth())
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines.",
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFD0BCFF)).padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Image composable",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+
+                )
+                Spacer(Modifier.height(8.dp).fillMaxWidth())
+                Text(
+                    "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+        Row(
+            modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Row() {
+                Column(
+                    modifier
+                        .fillMaxHeight()
+                        .weight(1f)
+                        .background(Color(0xFFB69DF8)).padding(horizontal = 12.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Row composable",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
+
+                    )
+                    Spacer(Modifier.height(8.dp).fillMaxWidth())
+                    Text(
+                        "A layout composable that places its children in a horizontal sequence.",
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+                Column(
+                    modifier
+                        .fillMaxHeight()
+                        .weight(1f)
+                        .background(Color(0xFFF6EDFF)).padding(horizontal = 12.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Column composable",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
+
+                    )
+                    Spacer(Modifier.height(8.dp).fillMaxWidth())
+                    Text(
+                        "A layout composable that places its children in a vertical sequence.",
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Exercici4(modifier: Modifier = Modifier) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
+        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow){
+            end.linkTo(boxRed.start)
+            bottom.linkTo(boxRed.top)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen){
+            start.linkTo(boxRed.end)
+            top.linkTo(boxRed.bottom)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan){
+            top.linkTo(boxRed.bottom)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            bottom.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+    }
+}
+
+@Composable
+fun Exercici5(modifier: Modifier){
+    Column (modifier.fillMaxSize()) {
+        Text(
+            "Jennifer Doe",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
+    Column(modifier.fillMaxSize()) {
+        Text("Android Developer Extraordinaire")
+    }
 }
 
 
 @Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    Exercici1()
-}
+    @Composable
+    fun GreetingPreview() {
+        Exercici5(modifier = Modifier)
+    }
